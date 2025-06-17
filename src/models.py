ee8e1e0ef3333aabd1bc44b4dfe7e0455f4bc17d
Zakaria.B <77070546+zakzaki244@users.noreply.github.com>
2025-06-17 19:22:19 -0400

    correction test route DAO

--- src/models.py
+++ src/models.py
@@ -5,6 +5,7 @@ import datetime
 
 class Store(Base):
     __tablename__ = "stores"
+    __table_args__ = {'extend_existing': True}
     id = Column(Integer, primary_key=True)
     name = Column(String, nullable=False, unique=True)
     products = relationship("Product", back_populates="store")
