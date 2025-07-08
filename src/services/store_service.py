69d5a7df8b94a7e33b4ed0a21a7a60c29f422f25
Zakaria.B <77070546+zakzaki244@users.noreply.github.com>
2025-07-07 22:20:37 -0400

    erreur identation

--- src/services/store_service.py
+++ src/services/store_service.py
@@ -8,10 +8,10 @@ from models.sale import Sale
 class StoreService:
     # --- Utilitaire : obtenir l'ID du centre logistique ---
     def get_centre_logistique_id():
-    from db.db import SessionLocal
-    from models.store import Store
-    session = SessionLocal()
-    centre = session.query(Store).filter_by(name="Centre Logistique").first()
-    centre_id = centre.id if centre else None
-    session.close()
-    return centre_id
+        from db.db import SessionLocal
+        from models.store import Store
+        session = SessionLocal()
+        centre = session.query(Store).filter_by(name="Centre Logistique").first()
+        centre_id = centre.id if centre else None
+        session.close()
+        return centre_id
