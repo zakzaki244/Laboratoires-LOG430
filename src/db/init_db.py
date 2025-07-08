4bc36dd3b9ebb9bc8d360854c2e1da55a1f2dba6
Zakaria.B <77070546+zakzaki244@users.noreply.github.com>
2025-07-08 09:45:33 -0400

    correction identation

--- src/db/init_db.py
+++ src/db/init_db.py
@@ -27,31 +27,31 @@ def init_db():
 
     # --- Ajout produits pour chaque magasin ---
     if session.query(Product).filter_by(store_id=magasin1.id).count() == 0:
-    produits1 = [
-        Product(name="Bouteille d'eau", category="Boisson", price=1.5, stock=50, store=magasin1),
-        Product(name="Chocolat", category="Snack", price=2.0, stock=30, store=magasin1),
-    ]
-    session.add_all(produits1)
-    session.commit()
+        produits1 = [
+            Product(name="Bouteille d'eau", category="Boisson", price=1.5, stock=50, store=magasin1),
+            Product(name="Chocolat", category="Snack", price=2.0, stock=30, store=magasin1),
+        ]
+        session.add_all(produits1)
+        session.commit()
 
     if session.query(Product).filter_by(store_id=magasin2.id).count() == 0:
-    produits2 = [
-        Product(name="Coca Cola", category="Boisson", price=1.5, stock=150, store=magasin2),
-        Product(name="Sandwich", category="Snack", price=2.0, stock=400, store=magasin2),
-    ]
-    session.add_all(produits2)
-    session.commit()
+        produits2 = [
+            Product(name="Coca Cola", category="Boisson", price=1.5, stock=150, store=magasin2),
+            Product(name="Sandwich", category="Snack", price=2.0, stock=400, store=magasin2),
+        ]
+        session.add_all(produits2)
+        session.commit()
 
     if session.query(Product).filter_by(store_id=centre.id).count() == 0:
-    produits_centre = [
-        Product(name="Coca Cola", category="Boisson", price=1.5, stock=500, store=centre),
-        Product(name="Sandwich", category="Snack", price=2.0, stock=300, store=centre),
-        Product(name="Oreo", category="Snack", price=2.0, stock=300, store=centre),
-        Product(name="Viande hachée", category="Boucherie", price=2.0, stock=300, store=centre),
-        Product(name="Détergent", category="Ménager", price=2.0, stock=300, store=centre),
-    ]
-    session.add_all(produits_centre)
-    session.commit()
+        produits_centre = [
+            Product(name="Coca Cola", category="Boisson", price=1.5, stock=500, store=centre),
+            Product(name="Sandwich", category="Snack", price=2.0, stock=300, store=centre),
+            Product(name="Oreo", category="Snack", price=2.0, stock=300, store=centre),
+            Product(name="Viande hachée", category="Boucherie", price=2.0, stock=300, store=centre),
+            Product(name="Détergent", category="Ménager", price=2.0, stock=300, store=centre),
+        ]
+        session.add_all(produits_centre)
+        session.commit()
 
     session.close()
     print("BD initialisée !")
