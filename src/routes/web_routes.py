ef9599211aa76e03c35b1d78028ad3522521e2c3
Zakaria.B <77070546+zakzaki244@users.noreply.github.com>
2025-07-07 22:21:16 -0400

    correction import

--- src/routes/web_routes.py
+++ src/routes/web_routes.py
@@ -4,7 +4,6 @@ from flasgger import Swagger
 from flask_cors import CORS
 from flask import Blueprint
 from services.product_service import ProductService
-from services.store_service import get_centre_logistique_id
 from services.store_service import StoreService
 from services.refund_service import RefundService
 from services.sale_service import SaleService
