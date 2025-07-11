8be8053fb5ef62da909a64ebe90605e8218d9cd9
Zakaria.B <77070546+zakzaki244@users.noreply.github.com>
2025-07-11 14:03:10 -0400

    correction erreur cache

--- src/app.py
+++ src/app.py
@@ -1,55 +1,48 @@
 from flask import Flask
 from flasgger import Swagger
 from flask_cors import CORS
-from routes.routes import api
-from routes.web_routes import web
-from db.init_db import init_db
 from prometheus_flask_exporter import PrometheusMetrics
 from prometheus_client import make_wsgi_app
 from werkzeug.middleware.dispatcher import DispatcherMiddleware
 from app.extensions import cache
+from db.init_db import init_db
 
+def create_app():
+    app = Flask(__name__)
+    CORS(app)
+    app.secret_key = "secret"
+    app.config['CACHE_TYPE'] = 'simple'
+    cache.init_app(app)
 
-app = Flask(__name__)
-CORS(app)
-app.secret_key = "secret"
-app.config['CACHE_TYPE'] = 'simple'
-cache.init_app(app)
+    swagger_template = {
+        "swagger": "2.0",
+        "info": {
+            "title": "A swagger API",
+            "version": "0.0.1"
+        },
+        "securityDefinitions": {
+            "Bearer": {
+                "type": "apiKey",
+                "name": "Authorization",
+                "in": "header",
+                "description": "Token d'authentification"
+            }
+        },
+        "security": [{"Bearer": []}]
+    }
+    Swagger(app, template=swagger_template)
 
+    # 💡 Import des routes après init
+    from routes.routes import api
+    from routes.web_routes import web
+    app.register_blueprint(api)
+    app.register_blueprint(web)
 
-swagger_template = {
-    "swagger": "2.0",
-    "info": {
-        "title": "A swagger API",
-        "version": "0.0.1"
-    },
-    "securityDefinitions": {
-        "Bearer": {
-            "type": "apiKey",
-            "name": "Authorization",
-            "in": "header",
-            "description": "Token d'authentification"
-        }
-    },
-    "security": [{"Bearer": []}]
-}
-swagger = Swagger(app, template=swagger_template)
+    # Prometheus
+    metrics = PrometheusMetrics(app)
+    metrics.info('app_info', 'LOG430 Flask App', version='4.0')
+    app.wsgi_app = DispatcherMiddleware(app.wsgi_app, {
+        '/metrics': make_wsgi_app()
+    })
 
-app.register_blueprint(api)
-app.register_blueprint(web)
-
-# Ajout de Prometheus
-metrics = PrometheusMetrics(app)
-metrics.info('app_info', 'LOG430 Flask App', version='4.0')
-
-# Ajout manuellement /metrics
-app.wsgi_app = DispatcherMiddleware(app.wsgi_app, {
-    '/metrics': make_wsgi_app()
-})
-
-
-
-
-if __name__ == "__main__":
-    init_db()
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    return app
