ded604902c0512aa71ad3bcce6cf7ccf4cc8f8cc
Zakaria.B <77070546+zakzaki244@users.noreply.github.com>
2025-07-16 10:21:06 -0400

    regle souci taille donnee bd

--- customer-service/infrastructure/db.py
+++ customer-service/infrastructure/db.py
@@ -6,7 +6,7 @@ class UserModel(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    password_hash = db.Column(db.String(128), nullable=False)
+    password_hash = db.Column(db.String(512), nullable=False)
     role = db.Column(db.String(50), nullable=False)
 
     def __init__(self, username, email, password_hash, role):
