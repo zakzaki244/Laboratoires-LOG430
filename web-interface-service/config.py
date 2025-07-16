a9ba5e119a9f91332c6d7f47d6b8d86656c0bb36
Zakaria.B <77070546+zakzaki244@users.noreply.github.com>
2025-07-16 11:05:16 -0400

    modif url

--- web-interface-service/config.py
+++ web-interface-service/config.py
@@ -2,5 +2,5 @@ import os
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY', 'web_secret_key')
-    API_GATEWAY_URL = os.environ.get('API_GATEWAY_URL', 'http://api-gateway:8080')
+    API_GATEWAY_URL = os.environ.get('API_GATEWAY_URL', 'http://10.194.32.174:8080')
     JWT_SECRET_KEY = os.environ.get('JWT_SECRET_KEY', 'jwt_secret_key')
