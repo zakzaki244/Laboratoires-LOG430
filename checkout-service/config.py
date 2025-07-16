f7797f206dca8e704cac43903e98d57d3202c28d
Zakaria.B <77070546+zakzaki244@users.noreply.github.com>
2025-07-16 10:16:10 -0400

    modification nom de base de donnee

--- checkout-service/config.py
+++ checkout-service/config.py
@@ -2,7 +2,7 @@ import os
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY', 'dev_secret')
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', 'postgresql://log430:laboratoire@10.194.32.174:5432/log430')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', 'postgresql://log430:laboratoire@10.194.32.174:5432/checkoutdb')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     JWT_SECRET_KEY = os.environ.get('JWT_SECRET_KEY', 'jwt_secret')
     
--- product-service/config.py
+++ product-service/config.py
@@ -2,6 +2,6 @@ import os
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY', 'dev_secret')
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', 'postgresql://log430:laboratoire@10.194.32.174:5432/log430')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', 'postgresql://log430:laboratoire@10.194.32.174:5432/productdb')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     JWT_SECRET_KEY = os.environ.get('JWT_SECRET_KEY', 'jwt_secret')
